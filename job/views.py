e5fa298c89a388bf9e4fd21067f6b9778cbb3317
Your Name <you@example.com>
2023-03-25 15:24:44 +0200

    Add paginator to the site

--- job/views.py
+++ job/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import JOb
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def job_list(resquest):
     job_list = JOb.objects.all()
-    context = {'jobs' : job_list}
+
+    paginator = Paginator(job_list, 1)
+    page_number = resquest.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+
+    context = {'jobs' : page_obj}
     return render(resquest,"job/job_list.html",context)
 
 def job_detail(resquest, id):
