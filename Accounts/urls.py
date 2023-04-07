4308e90f5cdf9842490d2bcc144639c3f7e8756f
Your Name <you@example.com>
2023-04-07 17:36:58 +0200

    Edit Profile

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -6,5 +6,8 @@ app_name = 'job'
 
 urlpatterns = [
     path('signup',views.signup,name='signup'),
+    path('profile',views.profile,name='profile'),
+    path('profile/edit',views.profile_edit,name='profile_edit'),
+
    
 ]
